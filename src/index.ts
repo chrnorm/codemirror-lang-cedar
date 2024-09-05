312d3ef83efe19eb48942207d2ddb411af23349f
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-09-05 17:32:18 +0100

    refactor + add tests

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ export const cedarLanguage = LRLanguage.define({
         String: t.string,
         LineComment: t.lineComment,
         "( )": t.paren,
+        "{ }": t.paren,
       }),
     ],
   }),
